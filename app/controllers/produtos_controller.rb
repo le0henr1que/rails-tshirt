08f8adf02f9dc8d41cb33df107ad11706781d204
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-08-03 19:21:50 -0300

    feat: layouts

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,4 +4,11 @@ class ProdutosController < ApplicationController
        @produtos_por_nome = Produto.order :nome
     end
 
+    def create
+
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+
+        produto = Produto.create valores
+    end
+
 end
